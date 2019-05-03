f39a6bac6b7ba079a76210bf25a095c7189f8521
Mark Mendel <mgm7734@gmail.com>
2019-05-03 09:14:26 -0500

    Initial commit, pc version

---
+++ init-pc.el
@@ -0,0 +1,17 @@
+(load "~/.emacs.d/shared-init")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(package-selected-packages
+   (quote
+    (color-theme-sanityinc-solarized color-theme-modern color-theme-sanityinc-solarizedcolor-theme-sanityfinc-solarized sanityinc-solarized-dark frame-fns use-package)))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
