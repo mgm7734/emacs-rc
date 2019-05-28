825e675d4bfe127edaf81e3ed5e97ce7f5c32e78
Mark Mendel <mgm7734@gmail.com>
2019-05-28 09:47:12 -0500

    pc font change

--- init-pc.el
+++ init-pc.el
@@ -10,8 +10,6 @@
     (color-theme-sanityinc-solarized color-theme-modern color-theme-sanityinc-solarizedcolor-theme-sanityfinc-solarized sanityinc-solarized-dark frame-fns use-package)))
  '(show-paren-mode t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
+ ;;'(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+ ;; 11pt -> 113, 10pt ->98
+ '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 113  :width normal)))))
