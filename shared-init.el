71eebd52d8b1775781470c65d6942476afd1cd30
Mark Mendel <mgm7734@gmail.com>
2020-10-20 08:24:07 -0500

    iblah

--- shared-init.el
+++ shared-init.el
@@ -440,7 +440,7 @@
   :ensure
   :mode "\\.ts")
 
-(use-package tide  :disable
+(use-package tide  :disabled
   :ensure t
   :after (typescript-mode company flycheck)
   :hook ((typescript-mode . tide-setup)
